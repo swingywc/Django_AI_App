b74ade70cb9e9da34cc483b08f4f54efa45710b1
swingywc <swingywc@gmail.com>
2022-08-26 09:29:01 +0800

    Add sample models into demo_app

--- DI_project/demo_app/models.py
+++ DI_project/demo_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class School(models.Model):
+    # 學派名稱
+    school_name = models.CharField(max_length=20)
+    # 中心價值
+    core_value = models.CharField(max_length=100)
+    # 人數
+    num_member = models.IntegerField()
+
+class Ancient_People(models.Model):
+    # 學派
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    # 名字
+    name = models.CharField(max_length=10)
+    # 名言
+    statement = models.CharField(max_length=100)
+    # 歲數
+    age = models.IntegerField()
